6a1724b111640875360cadd156052a49116db43c
yangsungsim227 <yangsungsim227@gmail.com>
2019-12-04 22:01:15 +0900

    Post and Get useage

---
+++ jsp_20191203/src/survlets/DemoServlet.java
@@ -0,0 +1,54 @@
+package survlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DemoServlet
+ */
+@WebServlet(
+		asyncSupported = true, 
+		description = "데모 서블릿", 
+		urlPatterns = { 
+				"/DemoServlet", 
+				"/demo.do"
+		}, 
+		initParams = { 
+				@WebInitParam(name = "Servlet", value = "4.0", description = "servlet 4.0")
+		})
+public class DemoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DemoServlet() {
+        super();
+        System.out.println("서블릿 기본생성자");
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("doget");
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("post");
+		doGet(request, response);
+	}
+
+}
