8b81b9f325def9657392bfa2c26ac45dd3109b15
yangsungsim227 <yangsungsim227@gmail.com>
2019-12-26 22:24:38 +0900

    201912-26

---
+++ jsp_20191224/src/jsp_20191224/dao/MemberDAO.java
@@ -0,0 +1,5 @@
+package jsp_20191224.dao;
+
+public interface MemberDAO {
+
+}
---
+++ jsp_20191224/src/jsp_20191224/dao/MemberDAOImpl.java
@@ -0,0 +1,13 @@
+package jsp_20191224.dao;
+
+import domain.MemberVO;
+
+public class MemberDAOImpl implements MemberDAO {
+
+	@Override
+	public boolean insertMember(MemberVO member) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
